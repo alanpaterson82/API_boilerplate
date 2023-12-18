64311aebc3cf9efeb502a51fe050f84294db9abf
alanpaterson82 <>
2023-12-18 13:02:43 +0000

    save first lecture

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const API_KEY = "troULWIkwycLjw-QT4gd8_Dj46w";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
