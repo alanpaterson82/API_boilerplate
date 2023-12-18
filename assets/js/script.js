439b58e697827ac025876fee4a4ee1048491c177
alanpaterson82 <>
2023-12-18 13:47:22 +0000

    save making a POST request part 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,35 @@ async function postForm(e) {
                                         "Authorization": API_KEY,
         },                                
                                 body: form,
-    })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+    
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class ="no errors">No errors reported!></div>`;
+    } else {
+        results = `<div>TotalErrors: <span class="error_count">${data.total_errors}</span>/div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class ="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 async function getStatus(e) {
